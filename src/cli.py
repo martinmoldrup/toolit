e7c59c94a64a03a26b54da8fa70a01c9846b6028
Martin Møldrup <14809761+martinmoldrup@users.noreply.github.com>
2025-05-21 17:28:09 +0200

    Add basic tests and change default path of tools

--- src/cli.py
+++ src/cli.py
@@ -2,7 +2,7 @@ import pathlib
 from .auto_loader import load_tools_from_folder
 from .create_apps_and_register import app
 
-PATH = pathlib.Path("experimentation") / "tools"
+PATH = pathlib.Path() / "devtools"
 load_tools_from_folder(PATH)
 
 if __name__ == "__main__":
---
+++ tests/cli_test.py
@@ -0,0 +1,14 @@
+import src.create_apps_and_register as create_apps_and_register
+from typer.testing import CliRunner
+
+def a_new_command_registered() -> None:
+    """Test a new command is registered."""
+    print("This is a new command registered.")
+    
+def test_cli_command_is_regitered() -> None:
+    # Get the commands from the typer cli app
+    create_apps_and_register.register_command(a_new_command_registered, name="a-new-command-registered")
+    runner = CliRunner()
+    result = runner.invoke(create_apps_and_register.app, ["--help"])
+    assert result.exit_code == 0
+    assert "a-new-command-registered" in result.stdout

502f32c15080507987ac92ee3b92d7ff543a3ad7
Martin Møldrup <mmoeldrup@grundfos.com>
2025-05-21 17:28:09 +0200

    Add basic tests and change default path of tools

---
+++ tests/mcp_server_test.py
@@ -0,0 +1,14 @@
+import pytest
+import src.create_apps_and_register as create_apps_and_register
+
+
+@pytest.mark.asyncio
+async def test_mcp_server() -> None:
+    """Test the mcp server tool is available."""
+    def some_tool() -> None:
+        """Tool for testing purpose."""
+        pass
+    create_apps_and_register.register_command(some_tool, name="some_tool")
+    tools = await create_apps_and_register.mcp.list_tools()
+    tool_instance = [tool for tool in tools if tool.name == "some_tool"][0]
+    assert bool(tool_instance) is not False

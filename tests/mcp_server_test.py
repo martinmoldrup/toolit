384f0d308995537b12cec9ee112f6f4cae4750f5
Martin Møldrup <14809761+martinmoldrup@users.noreply.github.com>
2025-06-16 16:31:36 +0200

    Make the mcp test only run if mcp is installed

--- tests/mcp_server_test.py
+++ tests/mcp_server_test.py
@@ -1,10 +1,20 @@
+"""Test for mcp server tool availability."""
+
 import pytest
+import importlib.util
 import toolit.create_apps_and_register as create_apps_and_register
 
+mcp_spec = importlib.util.find_spec("mcp")
+mcp_installed: bool = mcp_spec is not None
 
+
+@pytest.mark.skipif(not mcp_installed, reason="mcp package is not installed")
 @pytest.mark.asyncio
 async def test_mcp_server() -> None:
     """Test the mcp server tool is available."""
+    assert create_apps_and_register.mcp is not None, (
+        "This test should only run if mcp is installed. create_apps_and_register.mcp is None indicating it is not installed."
+    )
 
     def some_tool() -> None:
         """Tool for testing purpose."""
@@ -12,5 +22,5 @@ async def test_mcp_server() -> None:
 
     create_apps_and_register.register_command(some_tool, name="some_tool")
     tools = await create_apps_and_register.mcp.list_tools()
-    tool_instance = [tool for tool in tools if tool.name == "some_tool"][0]
+    tool_instance = next(tool for tool in tools if tool.name == "some_tool")
     assert bool(tool_instance) is not False
